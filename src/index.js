77ed66f5ef6a745ecee5c972ba5a1e7ded0f4e5b
Stephan Sum <sum@live.de>
2017-03-18 20:16:32 +0100

    configured default routing setup

--- src/index.js
+++ src/index.js
@@ -2,17 +2,16 @@ import "./index.scss";
 
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
+import App from "./components/App.js";
+import Home from "./components/Home.js";
 
 
-
-var App = React.createClass({
-    render: function() {
-        return (
-            <h1>React is up and running :-)</h1>
-        );
-    }
-});
-
-
-ReactDOM.render(<App />, document.getElementById("app"));
+ReactDOM.render((
+    <Router>
+        <App>
+            <Route path="/" component={Home}/>
+        </App>
+    </Router>)
+, document.getElementById("app"));
